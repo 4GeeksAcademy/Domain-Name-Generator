f9dc8e37758177e980919ddc900232b94698512a
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-06 10:04:47 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,15 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+//write your code here
+window.onload = () => {
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("domain").innerHTML = domGenerator();
+  });
 
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let domain = ["com, es"];
+  let dom = ["es", "com"];
 
-  for (i = 0; i < pronoun.length; i++)
-    for (i = 0; i < pronoun.length; i++)
-      console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          console.log(`${pronoun[i]}${adj[j]} ${noun[k]}${dom[l]}`);
+        }
+      }
+    }
+  }
 };
