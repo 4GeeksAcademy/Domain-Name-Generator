01843c921e72f8acdd8763401680c740b526acdf
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-09 08:37:38 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,22 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//write your code here
-window.onload = () => {
-  document.getElementById("btn").addEventListener("click", () => {
-    document.getElementById("domain").innerHTML = domGenerator();
-  });
-
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let dom = ["es", "com"];
+window.onload = function() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dom = [".com", ".es", ".it"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < dom.length; l++) {
-          console.log(`${pronoun[i]}${adj[j]} ${noun[k]}${dom[l]}`);
+          console.log(`${pronoun[i]} ${adj[j]} ${noun[k]} ${dom[l]}`);
         }
       }
     }
