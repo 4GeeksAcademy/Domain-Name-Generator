63158382220bd3b13e0fdc6b3f46a17df3f0d9f9
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-10 08:11:23 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,18 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("dom").innerHTML = donGenerator();
+  });
+
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
   var noun = ["jogger", "racoon"];
   var dom = [".com", ".es", ".it"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < dom.length; l++) {
-          console.log(`${pronoun[i]} ${adj[j]} ${noun[k]} ${dom[l]}`);
+  let donGenerator = function(pronoun, adj, noun, dom) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            console.log(`${pronoun[i]} ${adj[j]} ${noun[k]} ${dom[l]}`);
+          }
         }
       }
     }
-  }
+  };
+
+  donGenerator(pronoun, adj, noun, dom);
 };
